210d3de2a2e44f0011b8f3342b9af460a986013a
Yxxxb <2947934317@qq.com>
2021-12-20 20:35:02 +0800

    feat : 搜索课程

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Test from "@/pages/Test/index"
 import test1 from "@/pages/Test/test1";
 import test2 from "@/pages/Test/test2";
 import test3 from "@/pages/Test/test3";
+import pick from "@/pages/Test/pick";
 import examTest from "@/pages/Test/examTest";
 
 // 创建一个路由器 并暴露
@@ -33,6 +34,10 @@ const router = new VueRouter({
                 {
                     path: "/test3",
                     component: test3
+                },
+                {
+                    path: "/pick",
+                    component: pick
                 }
             ]
         },
