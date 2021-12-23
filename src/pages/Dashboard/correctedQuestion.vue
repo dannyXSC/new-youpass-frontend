91a29da8ebf17b620de22e5abbab17a1f5bc410b
dannyXSC <874794016@qq.com>
2021-12-23 20:09:37 +0800

    local

--- src/pages/Dashboard/correctedQuestion.vue
+++ src/pages/Dashboard/correctedQuestion.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <page-title
-      :heading="heading"
-      :subheading="subheading"
-      :icon="icon"
-      :breadcrumb-item="breadcrumbItem"
+        :heading="heading"
+        :subheading="subheading"
+        :icon="icon"
+        :breadcrumb-item="breadcrumbItem"
     ></page-title>
 
     <my-list title="课程信息" :items="items" :fields="fields">
@@ -13,13 +13,17 @@
           <b-tabs pills card>
             <b-tab title="其他功能" active>
               <b-button
-                class="mr-2 mb-2"
-                variant="primary"
-                @click="handleManualCorrect(row.row.item)"
+                  class="mr-2 mb-2"
+                  variant="primary"
+                  @click="handleManualCorrect(row.row.item)"
               >
                 手动批改
               </b-button>
-              <b-button class="mr-2 mb-2" variant="info"> 自动批改 </b-button>
+              <b-button class="mr-2 mb-2" variant="info"
+                        @click="handleAutoCorrect(row.row.item)"
+              >
+                自动批改
+              </b-button>
             </b-tab>
           </b-tabs>
         </b-card>
@@ -31,7 +35,7 @@
 <script>
 import myList from "@/components/myList";
 import PageTitle from "@/layout/Components/PageTitle.vue";
-import { getUnmarkedQuestion } from "@/api";
+import {autoCorrect, getUnmarkedQuestion} from "@/api";
 import router from "@/router";
 
 export default {
@@ -51,34 +55,34 @@ export default {
       // courseId: 1000,
       // examId: 1
     })
-      .then((res) => {
-        console.log(res);
-        if (res.code === 100) {
-          if (Array.isArray(res.data)) {
-            res.data.forEach((value, index) => {
-              value._showDetails = false;
-              value.isActive = false;
-              this.items.push(JSON.parse(JSON.stringify(value)));
-            });
-            console.log(this.items);
+        .then((res) => {
+          console.log(res);
+          if (res.code === 100) {
+            if (Array.isArray(res.data)) {
+              res.data.forEach((value, index) => {
+                value._showDetails = false;
+                value.isActive = false;
+                this.items.push(JSON.parse(JSON.stringify(value)));
+              });
+              console.log(this.items);
+            } else {
+              alert("已经批改完成");
+              router.push("/dashboard");
+            }
           } else {
-            alert("已经批改完成");
+            alert("error");
             router.push("/dashboard");
           }
-        } else {
-          alert("error");
-          router.push("/dashboard");
-        }
-      })
-      .catch((error) => {
-        alert(error);
-      });
+        })
+        .catch((error) => {
+          alert(error);
+        });
   },
   data() {
     return {
       heading: "Standard Buttons",
       subheading:
-        "Wide selection of buttons that feature different styles for backgrounds, borders and hover options!",
+          "Wide selection of buttons that feature different styles for backgrounds, borders and hover options!",
       icon: "pe-7s-plane icon-gradient bg-tempting-azure",
       breadcrumbItem: [
         {
@@ -95,8 +99,8 @@ export default {
         },
       ],
       fields: [
-        { label: "题目id", key: "questionId" },
-        { label: "题干", key: "description" },
+        {label: "题目id", key: "questionId"},
+        {label: "题干", key: "description"},
         {
           label: "未批改人数",
           key: "restNumber",
@@ -118,6 +122,19 @@ export default {
         },
       });
     },
+    handleAutoCorrect(item) {
+      autoCorrect({
+        courseId: this.courseId,
+        examId: this.examId,
+        // courseId:1000,
+        // examId:1,
+        questionId: item.questionId,
+      }).then(res=>{
+        console.log(res)
+      }).catch(error=>{
+
+      })
+    }
   },
 };
 </script>
